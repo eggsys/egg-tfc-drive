5a62e984b06b9726d227e00d7b7432c5c0afc4d2
eggsys <egg.rkt2@gmail.com>
2021-06-01 03:08:06 +0700

    Main terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0f9d733050c9f5365"
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
