6d79544379892ae82f3bb55bc0b79ba432e4492a
eggsys <egg.rkt2@gmail.com>
2021-06-01 03:33:08 +0700

    edit var

--- variables.tf
+++ variables.tf
@@ -2,6 +2,12 @@ variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
   default     = "EGG-EC2_Instance"
-  db_write_capacity = 1
-  db_read_capacity = 2
+  
+}
+
+variable "db_write_capacity" {
+  description = "Value of the db"
+  type        = string
+  default     = "1"
+  
 }
